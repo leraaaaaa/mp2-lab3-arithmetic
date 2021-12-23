cad2279c30ee47adf01bc0c9cd3e6e59c9fa6d4d
leraaaaaa <76663486+leraaaaaa@users.noreply.github.com>
2021-12-23 17:52:05 +0300

    Update arithmetic.h

--- include/arithmetic.h
+++ include/arithmetic.h
@@ -1,1 +1,51 @@
-// объявление функций и классов для вычисления арифметических выражений
+// РѕР±СЉСЏРІР»РµРЅРёРµ С„СѓРЅРєС†РёР№ Рё РєР»Р°СЃСЃРѕРІ РґР»СЏ РІС‹С‡РёСЃР»РµРЅРёСЏ Р°СЂРёС„РјРµС‚РёС‡РµСЃРєРёС… РІС‹СЂР°Р¶РµРЅРёР№
+
+#ifndef __ARITHMETIC_H__
+#define __ARITHMETIC_H__
+
+#include <iostream>
+#include "stack.h"
+#include <string>
+
+class Arithmetic
+{
+private:
+	double x;
+	char op;
+	bool f;
+
+public:
+	Arithmetic()
+	{
+		f = true;
+		x = 1.0;
+	}
+
+	Arithmetic(const double& a)
+	{
+		f = 1;
+		x = a;
+	}
+
+	Arithmetic(const char& oper)
+	{
+		f = 0;
+		op = oper;
+	}
+
+	~Arithmetic()
+	{ }
+
+	void operator = (const double& a);
+	void operator = (const char& op);
+	void operator = (const Arithmetic& v);
+	double GetX() const;
+	char GetOp() const;
+	bool GetF() const;
+	int GetPriority();
+	double res(Arithmetic* m, int n);
+	double result(string s);
+	bool aaa(Arithmetic* m, int n);
+};
+
+#endif
