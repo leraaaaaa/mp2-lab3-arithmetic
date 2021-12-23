6a3647fac5688fe54a0d3343857231b8b39e9891
leraaaaaa <76663486+leraaaaaa@users.noreply.github.com>
2021-12-23 17:53:37 +0300

    Update stack.h

--- include/stack.h
+++ include/stack.h
@@ -1,9 +1,104 @@
-// объявление и реализация шаблонного стека
-// стек поддерживает операции: 
-// - вставка элемента, 
-// - извлечение элемента, 
-// - просмотр верхнего элемента (без удаления)
-// - проверка на пустоту, 
-// - получение количества элементов в стеке
-// - очистка стека
-// при вставке в полный стек должна перевыделяться память
+// РѕР±СЉСЏРІР»РµРЅРёРµ Рё СЂРµР°Р»РёР·Р°С†РёСЏ С€Р°Р±Р»РѕРЅРЅРѕРіРѕ СЃС‚РµРєР°
+// СЃС‚РµРє РїРѕРґРґРµСЂР¶РёРІР°РµС‚ РѕРїРµСЂР°С†РёРё: 
+// - РІСЃС‚Р°РІРєР° СЌР»РµРјРµРЅС‚Р°, 
+// - РёР·РІР»РµС‡РµРЅРёРµ СЌР»РµРјРµРЅС‚Р°, 
+// - РїСЂРѕСЃРјРѕС‚СЂ РІРµСЂС…РЅРµРіРѕ СЌР»РµРјРµРЅС‚Р° (Р±РµР· СѓРґР°Р»РµРЅРёСЏ)
+// - РїСЂРѕРІРµСЂРєР° РЅР° РїСѓСЃС‚РѕС‚Сѓ, 
+// - РїРѕР»СѓС‡РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЌР»РµРјРµРЅС‚РѕРІ РІ СЃС‚РµРєРµ
+// - РѕС‡РёСЃС‚РєР° СЃС‚РµРєР°
+// РїСЂРё РІСЃС‚Р°РІРєРµ РІ РїРѕР»РЅС‹Р№ СЃС‚РµРє РґРѕР»Р¶РЅР° РїРµСЂРµРІС‹РґРµР»СЏС‚СЊСЃСЏ РїР°РјСЏС‚СЊ
+
+#ifndef __STACK_H__
+
+#define __STACK_H__
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+
+class TStack
+{
+private:
+	T* stack;
+	int head, size;
+  
+public:
+	TStack()
+	{
+		stack = new T[100];
+		head = 0;
+		size = 100;
+	}
+
+	TStack(int n)
+	{
+		if (n < 0)
+			throw "incorrect stack size";
+		stack = new T[n];
+		head = 0; 
+		size = n;
+	}
+
+	TStack(const TStack& a)
+	{
+		size = a.size;
+		head = a.head;
+		stack = new T[size];
+		for (int i = 0; i < size; i++)
+			stack[i] = a.stack[i];
+	}
+
+	~TStack()
+	{
+		delete[] stack;
+	}
+
+	void push(T& A)
+	{
+		if (head == size)
+		{
+			T* a = stack;
+			stack = new T[2 * size];
+			for (int i = 0; i < size; i++)
+				stack[i] = a[i];
+			delete[] a;
+			size = 2 * size;
+		}
+		stack[head++] = A;
+	}
+
+	T pop()
+	{
+		if (IsEmpty()) throw "it is not possible to use pop with an empty stack";
+		head--;
+		return stack[head];
+	}
+
+	bool IsEmpty()
+	{
+		if (head == 0) return true;
+		else return false;
+	}
+
+	void clear()
+	{
+		delete[] stack;
+		stack = new T[size];
+		head = 0;
+	}
+
+	int GetHead()
+	{
+		return head;
+	}
+
+	T peek()
+	{
+		if (IsEmpty()) throw "it is not possible to use peek with an empty stack";
+		return stack[head - 1];
+	}
+};
+
+#endif
