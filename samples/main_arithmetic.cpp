917cac5858be611420705f5b2d893fd0238db373
leraaaaaa <76663486+leraaaaaa@users.noreply.github.com>
2021-12-23 17:50:11 +0300

    Update main_arithmetic.cpp

--- samples/main_arithmetic.cpp
+++ samples/main_arithmetic.cpp
@@ -1,6 +1,24 @@
 // реализация пользовательского приложения
 
+#include <..\include\arithmetic.h>
+#include <iostream>
+
+using namespace std;
+
 int main()
 {
+	try
+	{
+		string s;
+		cout << "Please, enter math expression... " << endl;
+		cin >> s;
+		Arithmetic a;
+		double f = a.result(s);
+		cout << "Result = " << f << endl;
+	}
+	catch (char* error)
+	{
+		cout << error << endl;
+	}
   return 0;
 }
